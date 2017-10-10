57572c25ab469cc1d1f5a3bf95873b2ca23d7015
jaehoon166 <jaehoon166@gmail.com>
2017-10-10 14:41:06 +0900

    '.'

--- java-basic/src/main/java/bitcamp/java100/Test15_1.java
+++ java-basic/src/main/java/bitcamp/java100/Test15_1.java
@@ -9,7 +9,7 @@ public class Test15_1 {
       System.out.printf("10 * 20 = %d\n", 10 * 20);
       System.out.printf("5.0 / 2.0 = %.1f\n", 5.0 / 2.0);
       System.out.printf("5 / 2 = %d\n", 5 / 2);
-      //System.out.printf("10 % 20 = %d\n", 10 % 20);
+      //System.out.printf("10 % 20 = %d\n", 10 % 20);s
       System.out.printf("10 %% 20 = %d\n", 10 % 20);
     
   }
